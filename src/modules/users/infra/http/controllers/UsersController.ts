dbfa31e8047ba3c0f45f4632c4eb3b50cef9de1c
Djamilson <djamilson@gmail.com>
2020-08-20 20:19:59 -0300

    Initial

---
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,20 @@
+import { classToClass } from 'class-transformer';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+export default class UserController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    try {
+      const { name, email, password } = req.body;
+      const createUser = container.resolve(CreateUserService);
+
+      const user = await createUser.execute({ name, email, password });
+
+      return res.json(classToClass(user));
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
+}
---
+++ src/shared/container/providers/index.ts
@@ -0,0 +1,9 @@
+import './StorageProvider';
+
+import './MailTemplateProvider';
+
+// erro aqui, já coloque em vários lugares, e continua o mesmo erro
+import './MailProvider';
+// ao inseria a dependência
+
+import './CacheProvider';
